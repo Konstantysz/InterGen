ad5acee9cadb3e9ed499c8c181e33d78e41ffdbb
konstantysz <kostekszumigaj@gmail.com>
2021-03-31 12:55:05 +0200

    Working reading settings of genreation from json file

--- InterGen.py
+++ InterGen.py
@@ -1,25 +1,41 @@
 import time
-import math
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 from InterferogramGenerator import InterferogramGenerator, InterferogramFromRandomPolynomials
 from generateRandomPolynomial import generateRandomPolynomial
 
-def InterGen(folder, imSize = 512, numberOfImages = 1000):
-    InGen = InterferogramFromRandomPolynomials(imSize)
-    InGen.setFrequencyBoundaries(15, 30)
-    InGen.setOrientationBoundaries(0, math.pi)
-    InGen.generateALODI(3, 3, numberOfImages, folder)
+def InterGen(results_folder):
+    settings_file = open('settings.json',)
+    data = json.load(settings_file)
+    settings_file.close()
+    
+    InGen = InterferogramFromRandomPolynomials(data['Image_Size'])
+    InGen.setFrequencyBoundaries(
+        data['Frequencies']['Min_Frequency'], 
+        data['Frequencies']['Max_Frequency']
+        )
+    InGen.setOrientationBoundaries(
+        data['Orientation']['Min_Orientation_Angle'], 
+        data['Orientation']['Max_Orientation_Angle']
+        )
+    InGen.generateALODI(
+        data['Frequencies']['Frequencies_Per_Object'], 
+        data['Orientation']['Orientations_Per_Object'], 
+        data['Number_Of_Images'], 
+        results_folder
+        )
+
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    InterGen('C:\\Users\\koste\\Documents\\Python-Source-Codes\\InterGen\\Results\\', 512, 900)
+    InterGen('.\\Results\\')
     print("Execution time: %.2f sec" % (time.time() - start_time))
 
 
 # Dodać sfere i funkcje liniowe
 # losowo czestości i orientacje <- Done
 # zrobić porządne readme.md <- WIP
-# dawać różne tło
+# dawać różne tło -> Zakres tła od 0 do jakiejś liczby żeby nie było prześwietlenia czy czegoś
