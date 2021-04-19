fdd33d44f4ceac5b8e997020ed7f44e0eff90fb2
konstantysz <kostekszumigaj@gmail.com>
2021-04-19 10:29:43 +0200

    Reconfigured gui

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -18,43 +18,65 @@ tabControl.pack(expand = 1, fill ="both")
 ###
 ### TAB 1 - GENERATOR
 ###
-ttk.Label(tab1, text ="Please type your generation settings").grid(column = 0, row = 0, columnspan = 2, padx = 5, pady = 5)
-ttk.Label(tab1, text ="General settings").grid(column = 0, row = 1, columnspan = 2, padx = 5, pady = 5)
 
-### Image size entry
+# Variables for storing input
 image_size = tk.IntVar(tab1, 512)
-l1 = ttk.Label(tab1, text ="Image Size", anchor = 'e').grid(column = 0, row = 2)
-e1 = ttk.Entry(tab1, textvariable = image_size).grid(column = 1, row = 2)
-
-### Image number
 image_number = tk.IntVar(tab1, 10)
-l2 = ttk.Label(tab1, text ="Number of images to generate", anchor = 'e').grid(column = 0, row = 3, padx = 5, pady = 5)
-e2 = ttk.Entry(tab1, textvariable = image_number).grid(column = 1, row = 3, padx = 5, pady = 5)
-
-ttk.Label(tab1, text ="Advanced settings (Recommended to leave default)").grid(column = 0, row = 4, padx = 5, pady = 5, columnspan = 2)
-
-### Frequency settings
 min_freq = tk.IntVar(tab1, 15)
 max_freq = tk.IntVar(tab1, 30)
-l3 = ttk.Label(tab1, text ="Minimal spatial frequency", anchor = 'e').grid(column = 0, row = 5,padx = 5, pady = 5)
-e3 = ttk.Entry(tab1, textvariable = min_freq).grid(column = 1, row = 5, padx = 5, pady = 5)
-l4 = ttk.Label(tab1, text ="Maximal spatial frequency", anchor = 'e').grid(column = 0, row = 6,padx = 5, pady = 5)
-e4 = ttk.Entry(tab1, textvariable = max_freq).grid(column = 1, row = 6, padx = 5, pady = 5)
-
-### Orientation settings
 min_angle = tk.DoubleVar(tab1, 0.0)
 max_angle = tk.DoubleVar(tab1, 3.141592653589793)
-l5 = ttk.Label(tab1, text ="Minimal orientation angle", anchor = 'e').grid(column = 0, row = 7,padx = 5, pady = 5)
-e5 = ttk.Entry(tab1, textvariable = min_angle).grid(column = 1, row = 7, padx = 5, pady = 5)
-l6 = ttk.Label(tab1, text ="Maximal orientation angle", anchor = 'e').grid(column = 0, row = 8,padx = 5, pady = 5)
-e6 = ttk.Entry(tab1, textvariable = max_angle).grid(column = 1, row = 8, padx = 5, pady = 5)
 
-b1 = ttk.Button(tab1, text="Generate", command=callGenerator).grid(column = 0, row = 9, padx = 5, pady = 5, columnspan = 2)
+# Gui blocks
+h1 = ttk.Label(tab1, text ="Please type your generation settings")
+h2 = ttk.Label(tab1, text ="General settings")
+
+l1 = ttk.Label(tab1, text ="Image size:", anchor = 'w', width=40)
+e1 = ttk.Entry(tab1, textvariable = image_size)
+l2 = ttk.Label(tab1, text ="Number of images to generate:", anchor = 'w', width=40)
+e2 = ttk.Entry(tab1, textvariable = image_number)
+
+s1 = ttk.Label(tab1, text ="")
+h3 = ttk.Label(tab1, text ="Advanced settings (Recommended to leave default)")
+
+l3 = ttk.Label(tab1, text ="Minimal spatial frequency:", anchor = 'w', width=40)
+e3 = ttk.Entry(tab1, textvariable = min_freq)
+l4 = ttk.Label(tab1, text ="Maximal spatial frequency:", anchor = 'w', width=40)
+e4 = ttk.Entry(tab1, textvariable = max_freq)
+
+
+l5 = ttk.Label(tab1, text ="Minimal orientation angle:", anchor = 'w', width=40)
+e5 = ttk.Entry(tab1, textvariable = min_angle)
+l6 = ttk.Label(tab1, text ="Maximal orientation angle:", anchor = 'w', width=40)
+e6 = ttk.Entry(tab1, textvariable = max_angle)
+
+b1 = ttk.Button(tab1, text="Generate", command=callGenerator)
+progress = ttk.Progressbar(tab1, orient = tk.HORIZONTAL, length = 400, mode = 'determinate')
+
+# Grid positioning of blocks
+h1.grid(column = 0, row = 0, columnspan = 2, padx = 5, pady = 5)
+h2.grid(column = 0, row = 1, columnspan = 2, padx = 5, pady = 5)
+l1.grid(column = 0, row = 2)
+e1.grid(column = 1, row = 2)
+l2.grid(column = 0, row = 3, padx = 5, pady = 5)
+e2.grid(column = 1, row = 3, padx = 5, pady = 5)
+s1.grid(column = 0, row = 4, padx = 5, pady = 5, columnspan = 2)
+h3.grid(column = 0, row = 5, padx = 5, pady = 5, columnspan = 2)
+l3.grid(column = 0, row = 6,padx = 5, pady = 5)
+e3.grid(column = 1, row = 6, padx = 5, pady = 5)
+l4.grid(column = 0, row = 7,padx = 5, pady = 5)
+e4.grid(column = 1, row = 7, padx = 5, pady = 5)
+l5.grid(column = 0, row = 8,padx = 5, pady = 5)
+e5.grid(column = 1, row = 8, padx = 5, pady = 5)
+l6.grid(column = 0, row = 9, padx = 5, pady = 5)
+e6.grid(column = 1, row = 9, padx = 5, pady = 5)
+b1.grid(column = 0, row = 10, padx = 5, pady = 5, columnspan = 2)
+progress.grid(column = 0, row = 11, padx = 5, pady = 5, columnspan = 2)
 
 ###
 ### TAB 2 - RESULTS
 ###
-l7 = ttk.Label(tab2, text ="Results", anchor = 'e').grid(column = 0,row = 0, padx = 5, pady = 5, columnspan = 5)
+l7 = ttk.Label(tab2, text ="Results", anchor = 'w', width=40).grid(column = 0,row = 0, padx = 5, pady = 5, columnspan = 5)
 sb = ttk.Scrollbar(tab2).grid(column = 0,row = 1, padx = 5, pady = 5, columnspan = 2)
 
 
